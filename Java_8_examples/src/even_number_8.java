c0cf531e927e8e31d185a7ccffbc240627f494c3
shubhampatil <shubhampatil@192.168.1.50>
2022-08-09 13:53:46 +0530

    ver_1

---
+++ Java_8_examples/src/even_number_8.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class even_number_8 {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8);
+		list.stream().filter(n->n%2==0).forEach(System.out::println);
+		
+
+	}
+
+}
