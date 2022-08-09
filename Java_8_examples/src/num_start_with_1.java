cd97ecdfe184ba4f24d6519c275ffc0ea33d7bcb
shubhampatil <shubhampatil@192.168.1.50>
2022-08-09 22:38:40 +0530

    ver_3

---
+++ Java_8_examples/src/num_start_with_1.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class num_start_with_1 {
+
+	public static void main(String[] args) {
+		
+		
+		List<Integer> list = Arrays.asList(56,35,11,561,134,15,78);
+		
+		list.stream().map(n->n + "").filter(n-> n.startsWith("1")).forEach(System.out::println);
+		
+		
+		
+		
+		
+
+	}
+
+}
